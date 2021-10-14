3e6dcd5ad19cbfad35047f8ef4a3fec99a48fd3c
Skevin07 <63690856+Skevin07@users.noreply.github.com>
2021-10-13 18:00:50 -0600

    Completando clase
    Actualizando proyecto con los ultimos cambios hecho en el proyecto de la clase

--- app/src/main/java/com/unab/kevin/setget/Automovil.kt
+++ app/src/main/java/com/unab/kevin/setget/Automovil.kt
@@ -1,4 +1,39 @@
 package com.unab.kevin.setget
 
-class Automovil {
+class Automovil(mar:String,col:String,pre:Int)  {
+
+    //Estas son las caracteristicas de nuestro automovil
+    var marca = mar
+    var color = col
+    var precio = pre
+
+
+    //Le agregamos get y set a velocidadmax
+    var velocidadMax = 0
+        get() = field + 10
+        set(value) {
+            field = value
+        }
+
+//Estas son las funciones que tendra nuestro automovil
+
+    fun encender()
+    {
+        println("El auto se ha encendido")
+    }
+
+    fun apagar()
+    {
+        println("El auto se ha apagado")
+    }
+
+    fun acelerar()
+    {
+        println("El auto esta acelerando")
+    }
+
+    fun detener()
+    {
+        println("El auto se esta deteniendo")
+    }
 }
--- app/src/main/java/com/unab/kevin/setget/MainActivity.kt
+++ app/src/main/java/com/unab/kevin/setget/MainActivity.kt
@@ -8,7 +8,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-       empleado()
+        //Aqui es donde llamamos a las funciones que estan en otras clases
+        //al llamarlas aca hace que se puedan ejecutar
+
+
+       //empleado()
+        automovil()
 
 
     }
@@ -21,6 +26,27 @@ class MainActivity : AppCompatActivity() {
         println("La edad de la empleada es: "+e1.edad)
         e1.nombre="Laura"
         println("El nombre de nuestra empleada es: "+e1.nombre)
+    }
+
+
+
+    fun automovil()
+    {
+        var toyota: Automovil = Automovil("Toyota","Negro",4000)
+        println("El auto que tienes es un: "+toyota.marca)
+        println("El color del auto es: " +toyota.color)
+        println("El percio del auto es de $" +toyota.precio)
+        toyota.apagar()
+
+        //Agregamos esto posterior de haber agregado los het y set de la variable
+        toyota.velocidadMax = 120
+
+
+        //Aca solo lo hacemos para poner el estado el vehiculo claro que lo llamamos de la clase
+        var honda: Automovil = Automovil("honda", "gris",5000)
+        honda.encender()
+
+
     }
 
 }
